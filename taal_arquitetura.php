be74934cf5c170d6399f2c6b1f6d4472e77e6332
Thiago Aquino <aquinolima.thiago@gmail.com>
2020-10-23 22:56:14 -0300

    consulta bd

---
+++ taal_arquitetura.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.3
+ */
+
+/**
+ * Database `taal_arquitetura`
+ */
+<div class="error"><h1>Erro</h1><p><strong>Análise estática:</strong></p><p>1 erros foram encontrados durante análise.</p><p><ol><li>Uma expressão era esperada. (near "FROM" at position 8)</li></ol></p><p><strong>Comando SQL:</strong>  <a href="#" class="copyQueryBtn" data-text="SELECT  FROM `taal_arquitetura`.`orcamentos_site`">Copiar</a>
+<a href="./url.php?url=https%3A%2F%2Fdev.mysql.com%2Fdoc%2Frefman%2F5.7%2Fen%2Fselect.html" target="mysql_doc"><img src="themes/dot.gif" title="Documentação" alt="Documentação" class="icon ic_b_help"></a><a href="db_sql.php?sql_query=SELECT++FROM+%60taal_arquitetura%60.%60orcamentos_site%60&amp;show_query=1&amp;db=taal_arquitetura"><span class="nowrap"><img src="themes/dot.gif" title="Edita" alt="Edita" class="icon ic_b_edit">&nbsp;Edita</span></a>    </p>
+<p>
+SELECT  FROM `taal_arquitetura`.`orcamentos_site`
+</p>
+<p>
+    <strong>Mensagens do MySQL : </strong><a href="./url.php?url=https%3A%2F%2Fdev.mysql.com%2Fdoc%2Frefman%2F5.7%2Fen%2Fserver-error-reference.html" target="mysql_doc"><img src="themes/dot.gif" title="Documentação" alt="Documentação" class="icon ic_b_help"></a>
+</p>
+<code>#1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `taal_arquitetura`.`orcamentos_site`' na linha 1</code><br></div>
